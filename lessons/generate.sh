1db927db6bb6f7923b18a2330be04f5e049b5658
Andreas Jung <info@zopyx.com>
2019-02-02 11:21:03 +0100

    updated

--- lessons/generate.sh
+++ lessons/generate.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 set -x
-target=$PWD/docs/source/generated
+target=$PWD/generated
 
 git rm -fr $target
 rm -fr $target
 mkdir -p $target
 
-for d in lesson-*
+for d in lesson-bas*
 do
+    echo "*******************************************************************"
 	echo $d
     mkdir -p $target/$d
     cd $d 
