802343b7d48fc137cabaa09d184086b851be790d
Andreas Jung <info@zopyx.com>
2019-02-02 11:29:11 +0100

    added

--- lessons/generate.sh
+++ lessons/generate.sh
@@ -7,7 +7,7 @@ git rm -fr $generated_dir
 rm -fr $generated_dir
 mkdir -p $generated_dir
 
-for d in lesson-bas*
+for d in lesson-*
 do
     echo "*******************************************************************"
 	echo $d
