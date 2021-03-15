0a31361430ab790d17bfb9166a1996e376605ff5
Andreas Jung <info@zopyx.com>
2021-03-15 18:40:06 +0100

    updated

--- docker/start-server.sh
+++ docker/start-server.sh
@@ -4,7 +4,8 @@ export PATH=$PATH:\
 /:\
 /tmp/python/bin:\
 /speedata-publisher/bin:\
-/node_modules/.bin
+/node_modules/.bin:\
+/usr/AHFormatterV70_64
 
 hypercorn pp.server.server:app --bind 0.0.0.0:8000
 
