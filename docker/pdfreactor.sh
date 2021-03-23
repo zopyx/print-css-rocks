986f560453bb62d36e4a4ddc24c67e82592fe698
Andreas Jung <info@zopyx.com>
2021-03-23 19:37:26 +0100

    fixes for license file

--- docker/pdfreactor.sh
+++ docker/pdfreactor.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-ls -la /PDFreactor/lib
-java -Dcom.realobjects.pdfreactor.webservice.licenseKeyUrl=file:///licensekey.txt  -jar /PDFreactor/lib/pdfreactor.jar  -i $1 -o $2
+java  -jar /PDFreactor/lib/pdfreactor.jar  -i $1 -o $2 --licenseKey file:///licensekey.txt
